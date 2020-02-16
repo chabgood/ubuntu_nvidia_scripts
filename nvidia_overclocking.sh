b8fb1accd7361651abffa9343a1545e53024e23c
Chris Habgood <chabgood@gmail.com>
2020-02-16 11:15:36 -0600

    dymanic set card power to 85%

--- nvidia_overclocking.sh
+++ nvidia_overclocking.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=125
-nvidia-settings -a [gpu:1]/GPUGraphicsClockOffset[3]=125
-nvidia-settings -a [gpu:2]/GPUGraphicsClockOffset[3]=125
-nvidia-settings -a [gpu:3]/GPUGraphicsClockOffset[3]=125
-nvidia-settings -a [gpu:4]/GPUGraphicsClockOffset[3]=125
+nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=115
+nvidia-settings -a [gpu:1]/GPUGraphicsClockOffset[3]=115
+nvidia-settings -a [gpu:2]/GPUGraphicsClockOffset[3]=115
+nvidia-settings -a [gpu:3]/GPUGraphicsClockOffset[3]=115
+nvidia-settings -a [gpu:4]/GPUGraphicsClockOffset[3]=115
+nvidia-settings -a [gpu:5]/GPUGraphicsClockOffset[3]=115
+nvidia-settings -a [gpu:6]/GPUGraphicsClockOffset[3]=115
 
 nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=550
 nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffset[3]=550
 nvidia-settings -a [gpu:2]/GPUMemoryTransferRateOffset[3]=550
 nvidia-settings -a [gpu:3]/GPUMemoryTransferRateOffset[3]=550
 nvidia-settings -a [gpu:4]/GPUMemoryTransferRateOffset[3]=550
+nvidia-settings -a [gpu:5]/GPUMemoryTransferRateOffset[3]=550
+nvidia-settings -a [gpu:6]/GPUMemoryTransferRateOffset[3]=550
+
--- set_power.sh
+++ set_power.sh
@@ -1,18 +1,20 @@
-echo $1
-if [ "$1" == "mine" ]; then
-  sudo nvidia-smi -pl 180 -i 0
-  sudo nvidia-smi -pl 180 -i 1
-  sudo nvidia-smi -pl 180 -i 2
-  sudo nvidia-smi -pl 144 -i 3
-  sudo nvidia-smi -pl 144 -i 4
-  sudo nvidia-smi -pl 144 -i 5
-  sudo nvidia-smi -pl 160 -i 6
+#!/home/chabgood/.rbenv/shims/ruby
+output = `nvidia-smi --query-gpu=index,power.default_limit --format=csv,noheader`
+
+data = {}
+out = output.split("\n").map{ |n| n.split(',')}.map do |row|
+  index, power = *row
+  power = power.to_i
+  { index: index, power: power }
+end
+
+out.each do |row|
+ if ARGV[0] == 'mine'
+   row[:power] = row[:power] *0.85 # set power to 85%
+   d = `sudo nvidia-smi -i #{row[:index]} -pl #{row[:power]}`
+   p d
  else
-  sudo nvidia-smi -pl 225 -i 0
-  sudo nvidia-smi -pl 225 -i 1
-  sudo nvidia-smi -pl 225 -i 2
-  sudo nvidia-smi -pl 180 -i 3
-  sudo nvidia-smi -pl 180 -i 4
-  sudo nvidia-smi -pl 180 -i 5
-  sudo nvidia-smi -pl 200 -i 6
-fi
+   d = `sudo nvidia-smi -i #{row[:index]} -pl #{row[:power]}`	
+   p d
+ end
+end
